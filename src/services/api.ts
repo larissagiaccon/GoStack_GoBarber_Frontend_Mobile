c47ae7e0febabba8f95b717b50780dc73a37212c
larissagiaccon <larissa_souz@hotmail.com>
2021-04-13 21:57:40 -0300

    Header do Dashboard

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.50.196:3333',
+  baseURL: 'http://192.168.50.145:3333',
   // baseURL: 'http://localhost:3333',
 });
 
